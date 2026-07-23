dbd6861f8547d086c3995344bacf5ab9ed6206d5
DAO ANH DUNG <102934196+drzunk@users.noreply.github.com>
2026-07-23 22:13:18 +0700

    Refactor BaseTest to remove ExtentReportManager
    Removed ExtentReportManager references and added getDriver method for TestListener.

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -1,13 +1,9 @@
 package core;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.ITestResult;
 import org.testng.annotations.*;
-import utils.ExtentReportManager;
 import utils.WebUI;
 
 import java.lang.reflect.Method;
@@ -18,29 +14,23 @@ public class BaseTest {
     public WebDriver driver;
     public WebUI webUI;
 
-    @BeforeSuite
-    public void beforeSuite() {
-        ExtentReportManager.initReport();
+    // HÀM QUAN TRỌNG: Cung cấp WebDriver cho TestListener để chụp ảnh lúc Fail
+    public WebDriver getDriver() {
+        return driver;
     }
 
-    // QUAY LẠI MỞ TRÌNH DUYỆT TỪNG LẦN (Cho từng dòng Excel)
+    // ĐÃ XÓA SẠCH HÀM @BeforeSuite CHỨA initReport() Ở ĐÂY ĐỂ TRÁNH GHI ĐÈ!
+
     @BeforeMethod
     public void setupTestCase(Method method, Object[] args) {
-        String testName = method.getName();
-        if (args != null && args.length > 0) {
-            testName = "Tạo đơn: " + args[0].toString();
-        }
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
         options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
 
-        // --- BỔ SUNG ĐOẠN NÀY ĐỂ TẮT POPUP LƯU MẬT KHẨU ---
         java.util.Map<String, Object> prefs = new java.util.HashMap<>();
         prefs.put("credentials_enable_service", false);
         prefs.put("profile.password_manager_enabled", false);
         options.setExperimentalOption("prefs", prefs);
-        // -------------------------------------------------
 
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
@@ -48,11 +38,10 @@ public class BaseTest {
 
         webUI = new WebUI(driver);
     }
-    // QUAY LẠI ĐÓNG TRÌNH DUYỆT TỪNG LẦN (Đã dọn sạch rác code lỗi)
+
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
-            // Thay vì dùng ExtentReportManager, chỉ in ra console thôi:
             System.out.println("--- Đóng trình duyệt Chrome ---");
             driver.quit();
         }
@@ -60,7 +49,7 @@ public class BaseTest {
 
     @AfterSuite
     public void afterSuite() {
-        ExtentReportManager.flushReport();
+        // Đã gỡ ExtentReportManager.flushReport() ở đây (Giao cho TestListener.onFinish)
         try {
             java.io.File reportFile = new java.io.File("test-output/ExtentReport.html");
             if (reportFile.exists()) {
